f3d57ec30c2ea90b3734e3239a039b012dbb3536
Riham Elgammudi <151038196+RihamEl@users.noreply.github.com>
2024-11-15 21:53:45 +0000

    Login code

--- CI4-EcoTrack/app/Controllers/UserController.php
+++ CI4-EcoTrack/app/Controllers/UserController.php
@@ -15,4 +15,44 @@ class UserController extends ResourceController
         // // Return the list using the respond function
         return $this->respond($users);
     }
+
+
+    public function userLogin()
+    {
+       $users= new UserModel();
+       $email=$this->request->getJSON()->email;
+       $password=$this->request->getJSON()->password;
+       log_message("debug","Email:".$email."password:".$password);
+
+       $user_data= $user->login($email,$password);
+      log_message ("debug", "returned from stored procedure:".print_r($user_data,true));
+
+      if($user_data->user_id){
+        return $this->respond ([
+            "user_id"=> $user_data->user_id,
+            "token" => "access.user",
+            "code"=> "200" ],200,"application/json");
+      }else {
+        return $this->respond([
+            "message"=> "Invalid email or password "
+        ], 401);
+      }
+
+
+    }
+
+
+    public function delete ($id = null)
+    {
+        $user = $this->model->find($id);
+        if (!$user){
+            return $this->failNotFound("User not found");   
+        }
+        if ($this->model->delete($id)){
+            return $this->respondDeleted(["id => $id, 'message' => 'User deleted sucessfully.'"])
+        } else {
+            return $this->fail("Failed to delete user.");
+        }
+    }
+
 }
--- CI4-EcoTrack/app/Models/UserModel.php
+++ CI4-EcoTrack/app/Models/UserModel.php
@@ -7,6 +7,19 @@ class UserModel extends Model
     // params
     protected $table = 'users'; // Table name
     protected $primaryKey = 'user_id'; // primary key
-    protected $allowedFields = ['username', 'first_name', 'last_name']; // Accessible fields
+    protected $allowedFields = ['username', 'first_name', 'last_name'];// Accessible fields
     
+
+
+
+    public function login($email, $password){
+        $this->db->query("Call loginValidation(?,?,@p_user_id)",[$email,$password]);
+
+        $query= $this->db->query("SELECT @p_user_id as user_id",[$email,$password]);
+
+        log_message("debug","returned from UserModel::login". print_r($query->getRow(),true) );
+
+        return $query->getRow();
+
+    }
 }
