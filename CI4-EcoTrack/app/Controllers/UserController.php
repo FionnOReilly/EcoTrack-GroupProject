47e99e9b0534b7816b12b3d7424bb1dc8641d4c9
kellyqs <150041935+kellyqs@users.noreply.github.com>
2024-11-30 18:10:01 +0000

    Update UserController.php
    just formated

--- CI4-EcoTrack/app/Controllers/UserController.php
+++ CI4-EcoTrack/app/Controllers/UserController.php
@@ -1,4 +1,5 @@
-<?php namespace App\Controllers;
+<?php
+namespace App\Controllers;
 
 use CodeIgniter\RESTful\ResourceController;
 
@@ -7,7 +8,8 @@ class UserController extends ResourceController
     protected $modelName = 'App\Models\UserModel'; // Specify what model this controller uses
     protected $format = 'json'; // Specify format
 
-    public function index(){
+    public function index()
+    {
 
         // Retrieve all users from the database, you may also call stored procedures here
         $users = $this->model->findAll();
@@ -15,12 +17,25 @@ class UserController extends ResourceController
         return $this->respond($users);
     }
 
-public function registerUser() { $data = $this->request->getJSON(); // Ensure the data keys match the expected field names 
-$userData = [ 'first_name' => $data->first_name,
- 'last_name' => $data->last_name, 'email' => 
-$data->email, 'password' => $data->password ]; if ($this->model->insertUser($userData)) { return $this->
-respond(['status' => 'success', 'message' => 'Registration successful']); }
- else { return $this->respond(['status' => 'error', 'message' =>
- 'Registration failed'], 500); } 
- }
+    public function registerUser()
+    {
+        $data = $this->request->getJSON(); // Ensure the data keys match the expected field names 
+        $userData = [
+            'first_name' => $data->first_name,
+            'last_name' => $data->last_name,
+            'email' =>
+                $data->email,
+            'password' => $data->password
+        ];
+        if ($this->model->insertUser($userData)) {
+            return $this->
+                respond(['status' => 'success', 'message' => 'Registration successful']);
+        } else {
+            return $this->respond([
+                'status' => 'error',
+                'message' =>
+                    'Registration failed'
+            ], 500);
+        }
+    }
 }
